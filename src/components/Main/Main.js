eb2b9336b703bdfc023023a9c83efb665bfd92f0
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2021-07-19 23:55:24 +0300

    about-project

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavTab from "./NavTab";
+import AboutProject from "./AboutProject";
 
 function Main() {
   return (
     <main>
       <NavTab />
+      <AboutProject />
     </main>
   );
 }
