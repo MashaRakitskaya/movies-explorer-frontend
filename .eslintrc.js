bcb184cdfa6b25b70c264562bd88f743c116815e
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2021-07-20 23:07:56 +0300

    aboutProject and timeLine

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    jest: true,
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    'react/prop-types': ['off'],
+    'react/jsx-key': [
+      'warn',
+      { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true },
+    ],
+    'prettier/prettier': ['warn', {}, { usePrettierrc: true }],
+    'no-console': 'warn',
+    'spaced-comment': ['warn', 'always'],
+  },
+};
--- src/components/Main/NavTab.js
+++ src/components/Main/NavTab.js
@@ -1,23 +1,23 @@
-import React from "react";
-import landinglogo from "../../images/landing-logo.svg";
+import React from 'react';
+import landinglogo from '../../images/landing-logo.svg';
 
 function NavTab() {
   return (
-    <section className="navtab">
-      <div className="navtab__container">
-        <h1 className="navtab__title">
+    <section className='navtab'>
+      <div className='navtab__container'>
+        <h1 className='navtab__title'>
           Учебный проект студента факультета Веб-разработки.
         </h1>
-        <p className="navtab__text">
+        <p className='navtab__text'>
           Листайте ниже, чтобы узнать больше про этот проект и его создателя.
         </p>
-        <button className="navtab__button">Узнать больше</button>
+        <button className='navtab__button'>Узнать больше</button>
       </div>
 
       <img
-        className="navtab__img"
+        className='navtab__img'
         src={landinglogo}
-        alt="Планета из слов web"
+        alt='Планета из слов web'
       />
     </section>
   );
