6d798663a27f988ca3bb544fcf212fb5ec5db235
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2021-07-28 23:51:04 +0300

    burgerMenu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -4,31 +4,35 @@ import iconpersone from "../images/iconpersone.svg";
 
 function BurgerMenu({ open, close }) {
   return (
-    <div className={`burger-menu ${open ? "" : "burger-menu_open"}`}>
-      <button onClick={close} className='burger-menu__button-close' />
-      <div className='burger-menu__link-box'>
-        <NavLink className='burger-menu__link' exact to='/'>
-          Главная
-        </NavLink>
+    <div
+      className={`burger-menu-under ${open ? "" : "burger-menu-under_open"}`}
+    >
+      <div className={`burger-menu ${open ? "" : "burger-menu_open"}`}>
+        <button onClick={close} className='burger-menu__button-close' />
+        <ul className='burger-menu__link-box'>
+          <NavLink className='burger-menu__link' exact to='/'>
+            Главная
+          </NavLink>
 
-        <NavLink className='burger-menu__link' to='/movies'>
-          Фильмы
-        </NavLink>
+          <NavLink className='burger-menu__link' to='/movies'>
+            Фильмы
+          </NavLink>
 
-        <NavLink className='burger-menu__link' to='/saved-movies'>
-          Сохранённые фильмы
-        </NavLink>
+          <NavLink className='burger-menu__link' to='/saved-movies'>
+            Сохранённые фильмы
+          </NavLink>
+        </ul>
+        <button className='burger-menu__button'>
+          <img
+            className='burger-menu__button-img'
+            src={iconpersone}
+            alt='Иконка человека'
+          />
+          <NavLink to='/profile' className='burger-menu__button-anchor'>
+            Аккаунт
+          </NavLink>
+        </button>
       </div>
-      <button className='header-navigation__button'>
-        <img
-          className='header-navigation__button-img'
-          src={iconpersone}
-          alt='Иконка человека'
-        />
-        <NavLink to='/profile' className='header-navigation__button-anchor'>
-          Аккаунт
-        </NavLink>
-      </button>
     </div>
   );
 }
